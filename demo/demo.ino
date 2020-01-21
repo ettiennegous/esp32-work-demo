decd3a447202febe750dbe95800d2abde183b4a9
ettiennegous <ettiennegous@hotmail.com>
2020-01-21 23:07:33 +1100

    Add LDR example

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,5 +1,6 @@
 #define LED_PIN 27 /*ESP 32 D27 LHS Middle*/
 #define BTN_PIN 26 /* ESP 32 D26 LHS Middle*/
+#define LDR_PIN 25 /* ESP 32 D25 LHS Middle*/
 
 bool lightOn = false;
 
@@ -7,12 +8,24 @@ void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
+  pinMode(LDR_PIN, INPUT);
   
 }
 
 void loop() {
   Serial.println("Hello, world!");
   bool bBtnState = digitalRead(BTN_PIN);
+  int ldrValue = analogRead(LDR_PIN);
+  
+  if(ldrValue < 1000) {
+    lightOn = true;
+  }
+
+  if(ldrValue >= 4095) {
+    lightOn = false;
+  }
+  
+  Serial.println(ldrValue, DEC);
   if(bBtnState) { 
     Serial.println("Btn Press!");  
     lightOn = !lightOn;
