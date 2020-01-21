c33b1f8b42d46c49f8551b938a98f71bf86eaa3b
ettiennegous <ettiennegous@hotmail.com>
2020-01-21 22:31:07 +1100

    Add a button to the circuit to turn the LED on and off

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,15 +1,23 @@
 #define LED_PIN 27 /*ESP 32 D27 LHS Middle*/
+#define BTN_PIN 26 /* ESP 32 D26 LHS Middle*/
+
+bool lightOn = false;
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+  
 }
 
 void loop() {
   Serial.println("Hello, world!");
-  digitalWrite(LED_PIN, HIGH);
-  
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  bool bBtnState = digitalRead(BTN_PIN);
+  if(bBtnState) { 
+    Serial.println("Btn Press!");  
+    lightOn = !lightOn;
+    
+  }
+  digitalWrite(LED_PIN, lightOn);
+  delay(100);
 }
